baaf66313481bc4e01bc72d323cba22015ef9a79
ericktejaxun <erickteja@gmail.com>
2022-01-01 18:28:47 -0600

    Se ha arreglado el front end para todos los casos de analisis

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+import os
+from flask import Flask, flash, url_for, redirect, url_for, render_template, request, jsonify
 from flask import send_file, current_app as app
 import json
 from json import JSONEncoder
-import os
+from werkzeug.utils import secure_filename
+import pandas as pd
 
-EntornoGlobal = None
-ArbolAnterior = None
+UPLOAD_FOLDER = './archivos/'
+ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx', 'json'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def obtenerListaArchivos():
+    lista= os.listdir(os.path.join(app.config['UPLOAD_FOLDER']))
+    return lista
 
 
 def obtenerParametros(option):
@@ -29,11 +40,11 @@ def obtenerParametros(option):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', listaArchivos=obtenerListaArchivos())
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    return render_template("menu.html", listaArchivos=obtenerListaArchivos())
 
 @app.route("/reports", methods=["POST", "GET"])
 def reports():
@@ -49,9 +60,52 @@ def getParametros():
         option = request.form["option"]
     return jsonify(obtenerParametros(option))     
 
+@app.route("/cargarArchivo", methods=["POST"])
+def cargarArchivoEntrada():
+    if request.method == "POST":
+        if 'file' not in request.files:            
+            return jsonify({"codigo": 100, "mensaje": "No se ha encontadro el archivo."})
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"codigo": 100, "mensaje": "No se ha seleccionado el archivo."})
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))        
+        return jsonify({"codigo": 200, "mensaje": "Archivo " + file.filename +" almacenado y cargado correctamente.", "archivos" : obtenerListaArchivos(), "archivo":file.filename})
+
+
+@app.route("/getCampos", methods=["POST"])
+def cargarCampos():
+    if request.method == "POST":
+        nombreArchivo = request.form["archivo"]
+        return jsonify(obtenerEncabezados(nombreArchivo))
+
+
+def obtenerEncabezados(file):    
+    if '.csv' in file: # El archivo es un csv
+        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'])+file)
+        encabezados = df.columns.values.tolist()        
+        #return encabezados.to_json(orient="records") 
+        return encabezados
+
+    if '.xlsx' in file: #El archivo es un excel
+        df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'])+file)
+        encabezados = df.columns.values.tolist()
+        #return encabezados.to_json(orient="records") 
+        return encabezados
+
+    if '.xlsx' in file: #El archivo es un excel
+        df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'])+file)
+        encabezados = df.columns.values.tolist()
+        #return encabezados.to_json(orient="records") 
+        return encabezados
+
 @app.route("/descargar" , methods=["POST"])
 def descargar():      
     return redirect("/static/ast.gv.pdf")
+
+
 
 if __name__ == "__main__":    
     app.config['TEMPLATES_AUTO_RELOAD'] = True
